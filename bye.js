e3def5c33a30d3af12939dea55418d655e4b17bf
cotne1233 <81570358+cotne1233@users.noreply.github.com>
2023-10-18 18:37:51 +0400

    homework1

---
+++ bye.js
@@ -0,0 +1,46 @@
+let firstName = "cotne";
+let lastName = "zaalishvili";
+let userAge = "13";
+
+console.log(firstName);
+console.log(lastName);
+console.log(userAge);
+
+let Number1 = 30;
+if (Number1 <50) {
+  console.log("<50");  
+} 
+
+if (Number1 > 20) {
+    console.log("> 20");
+    
+} else {console.log(Error);
+    
+}
+
+let result = Number1 < 50 ? "< 50" : "looks like something happened";
+console.log(result);
+
+let A = "mariam"
+if (A = "mariam") {
+    console.log(true);
+    
+} else {console.log(false);
+    
+}
+let Name1 = "mariam";
+if (Name1 = "mariam") {
+    
+} else {console.log(true);
+    
+}
+if (Name1 < "mariam") {
+    
+} else {console.log(false);
+    
+}
+if (Name1 > "mariam") {
+    
+} else {console.log(false);
+    console.log(null);
+}
